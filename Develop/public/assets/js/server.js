fe4b0ec726d2e6df5fc283d42a3879fd2879708f
Nate Griggs <nateghsc09@gmail.com>
2021-04-22 20:51:19 -0500

    server and links work, now need to make the notes work

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,42 +1,63 @@
-
+//set up express and the link
 const express = require('express');
 const path = require('path');
+//going to need to use fs and maybe datebase
 const fs = require("fs");
 
+//sets express
 const app = express();
-
+//port
 var PORT = process.env.PORT || 3001;
 
+//formating data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const notes = [
-    {
-      routeName: 'yoda',
-      name: 'Yoda',
-      role: 'Jedi Master',
-      age: 900,
-      forcePoints: 2000,
-    },
-    {
-      routeName: 'darthmaul',
-      name: 'Darth Maul',
-      role: 'Sith Lord',
-      age: 200,
-      forcePoints: 1200,
-    },
-    {
-      routeName: 'obiwankenobi',
-      name: 'Obi Wan Kenobi',
-      role: 'Jedi Master',
-      age: 55,
-      forcePoints: 1350,
-    },
-  ];
+//link to public folder for files
+app.use(express.static('public'));
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(notes));
+
+app.post('api/notes', (req, res) => {
+    //setting up adding new notes
+   
+        const newNote = req.body;
+
+               // Sets max notes
+               let highestId = 99; 
+               // sets up our notes list
+               for (let i = 0; i < database.length; i++) {
+                   let individualNote = database[i];
+       
+                   if (individualNote.id > highestId) {
+                       // keeps the highes id on top
+                       highestId = individualNote.id;
+                   }
+               }
+               // adds 1 to the id of each new note, keeping them seperate
+               newNote.id = highestId + 1;
+               // pushes it to the database
+               database.push(newNote)
+       
+               // rewirtes our db.json file
+               fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+       
+                   if (err) {
+                       return console.log(err);
+                   }
+                   console.log("Your note was saved!");
+               });
+               // shows the most recent note
+               res.json(newNote);
+           });
+           //sets our datebase up
+           app.listen(PORT, function () {
+            console.log("App listening on PORT " + PORT);
+        });
