00e2f6e445c06cc7977d24862c6924e99110e819
Nate Griggs <nateghsc09@gmail.com>
2021-04-24 11:13:37 -0500

    more file cleanup

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var PORT = process.env.PORT || 3001;
 //formating data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
 
@@ -36,18 +37,18 @@ app.get("/api/notes", (req, res) => {
 });
 
 
- // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-  //req is request and res is response 
-  //arrow functions vs reg functions, arrow doesn't do any execution 
-  //this points to sepcific reference in regular
-  // functions and arrow functions will point it to the global 
-  app.post("/api/notes", (req, res) => {
+// API POST Requests
+// Below code handles when a user submits a form and thus submits data to the server.
+// In each of the below cases, when a user submits form data (a JSON object)
+// ...the JSON is pushed to the appropriate JavaScript array
+// (ex. User fills out a reservation request... this data is then sent to the server...
+// Then the server saves the data to the tableData array)
+// ---------------------------------------------------------------------------
+//req is request and res is response 
+//arrow functions vs reg functions, arrow doesn't do any execution 
+//this points to sepcific reference in regular
+// functions and arrow functions will point it to the global 
+app.post("/api/notes", (req, res) => {
 
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
@@ -74,30 +75,30 @@ app.get("/api/notes", (req, res) => {
     //returns the new note added to the user
     return res.json(newNote);
 
-  });
-  //gets input from html from user
-  //takes in an id of a note and sending it back 
-  //to the front end to appear on html, sending it back to fetch request
-  //this route is written by me, and it has to match the fetch route
-  //you have to go to this url, (/api/notes/:id) and get this data
-  app.delete("/api/notes/:id", (req, res) => {
+});
+//gets input from html from user
+//takes in an id of a note and sending it back 
+//to the front end to appear on html, sending it back to fetch request
+//this route is written by me, and it has to match the fetch route
+//you have to go to this url, (/api/notes/:id) and get this data
+app.delete("/api/notes/:id", (req, res) => {
     let noteId = req.params.id;
     let newId = 0;
     console.log(`Deleting note with id ${noteId}`);
     let db = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"));
     db = db.filter(currentNote => {
-      return currentNote.id != noteId;
+        return currentNote.id != noteId;
     });
     for (currentNote of db) {
-      currentNote.id = newId.toString();
-      newId++;
+        currentNote.id = newId.toString();
+        newId++;
     }
-   //chooses where to write the data, db.json, and stringify it so that the data will go through
+    //chooses where to write the data, db.json, and stringify it so that the data will go through
     fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(db));
     res.json(db);
-  });
+});
 
-           //sets our datebase up
-           app.listen(PORT, function () {
-            console.log("App listening on PORT " + PORT);
-        });
+//sets our datebase up
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
